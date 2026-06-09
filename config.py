0e1ec2903ea59bad9e2e192129ee09b9b168b3fe
studiodesignmanager <studiodesignmanager@gmail.com>
2026-06-09 19:29:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
 TOKEN = '8948621877:AAEULL2V05Y21ns2x2cAKQYjERUMDViGads'
-CHANNEL = '@interdatingservices'
-HEADERS = {'User-Agent': 'Mozilla/5.0'}
+CHANNEL = "@interdatingservices"
 
-LOGIN_URL = "https://findbride.com/login"
-LOGIN = "guestodessa1@gmail.com"
-PASSWORD = "8gr$DwXQz"
-
-LOGIN_DATA = {
-    "email": LOGIN,
-    "password": PASSWORD
+HEADERS = {
+    "User-Agent": "Mozilla/5.0"
 }
+
+LOGIN = "guestodessa1@gmail.com"
+PASSWORD = "8gr$DwXQz
